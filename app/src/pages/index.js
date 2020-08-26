124f24d49f0803fb5a98ca19f47110d750310cd1
ypk <yprasannakumar@gmail.com>
2020-08-26 10:39:43 +0100

    Added pages and updated routes

---
+++ app/src/pages/index.js
@@ -0,0 +1,11 @@
+import About from "./About";
+import Cart from "./Cart";
+import Login from "./Login";
+import NotFound from "./NotFound";
+
+export {
+    About,
+    Cart,
+    Login,
+    NotFound
+};
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, useLocation } from "react-router-dom";
-import NotFound from "../pages/NotFound";
-
-const TempComponent = () => {
-    const location = useLocation();
-    return <span>{location.pathname}</span>
-}
+import { About, Cart, Login, NotFound } from "../pages";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={TempComponent}/>
-                <Route exact path="cart" component={TempComponent}/>
-                <Route path="*" component={NotFound}/>
+            <Route exact path="/" />
+            <Route exact path="/cart" component={Cart}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/about" component={About}/>
+            <Route path="*" component={NotFound}/>
             </Switch>
         </BrowserRouter>
     ) 

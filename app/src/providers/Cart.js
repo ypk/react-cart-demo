fe3b9c5ad96e558437d1ce33eef74906ac7fd09c
ypk <yprasannakumar@gmail.com>
2020-08-27 08:01:02 +0100

    added functions for cart context provider

--- app/src/providers/Cart.js
+++ app/src/providers/Cart.js
@@ -3,14 +3,32 @@ import { CartContext } from "../contexts";
 
 const CartContextProvider = ({ children }) => {
     const initialState = {items: [], checkout: false};
+    const reducer = () => {};
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const addProduct = () => {}
-    const removeProduct = () => {}
-    const incrementProductCount = () => {}
-    const decrementProductCount = () => {}
-    const checkout = () => {}
-    const clearCart = () => {}
+    const addProduct = () => {
+        console.log("Product added to Cart");
+    }
+    
+    const removeProduct = () => {
+        console.log("Product removed from Cart");
+    }
+    
+    const incrementProductCount = () => {
+        console.log("Product quantity increased in Cart");
+    }
+    
+    const decrementProductCount = () => {
+        console.log("Product quantity decreased in Cart");
+    }
+    
+    const checkout = () => {
+        console.log("Checking out witt cart");
+    }
+
+    const clearCart = () => {
+        console.log("Emptying Cart");
+    }
 
     const cartData = {
         addProduct,

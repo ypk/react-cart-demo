da9871a1586f99fdc4ee57f953a40609a38acb55
ypk <yprasannakumar@gmail.com>
2020-08-27 08:23:37 +0100

    updated cart context reducer

--- app/src/helpers/reducer.js
+++ app/src/helpers/reducer.js
@@ -1,23 +1,40 @@
-const reducer = (state, action) => {
-    switch(action) {
+const reducer = (state, trigger) => {
+    const {type, data} = trigger;
+    switch(type) {
         case "addItem": 
+            if(!state.items.find(item => item.id === data) {
+                state.items.push({
+                    ...data,
+                    quantity: 1
+                });
+            }
             return {
-                ...state
+                ...state,
+                items: [...state.items]
             } 
         case "removeItem": 
+            const filteredItems = [...state.items.filter(item => item.id !== data)];
             return {
-                ...state
+                ...state,
+                items: [...filteredItems]
             }
         case "incrementItem": 
+            const item = [state.items.findIndex(item => item.id === data)]
+            state.items[item].quantity++;
             return {
-                ...state
+                ...state,
+                items: [...state.items]
             } 
         case "decrementItem": 
+            const item = [state.items.findIndex(item => item.id === data)]
+            state.items[item].quantity--;
             return {
-                ...state
-            }
+                ...state,
+                items: [...state.items]
+            } 
         case "checkOut": 
             return {
+                items: [],
                 checkout: true
             } 
         case "emptyCart": 
